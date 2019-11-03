39ac276efb12612835aec7b0d003885f85a66838
2paedev <dmartinezayuso@MacBook-Pro-de-Diego.local>
2019-11-03 15:57:19 +0100

    update env vars settings

--- alicante_history/settings/pro.py
+++ alicante_history/settings/pro.py
@@ -15,18 +15,18 @@ CORS_ORIGIN_WHITELIST = ()
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': os.environ['RDS_DB_NAME'],
-        'USER': os.environ['RDS_USERNAME'],
-        'PASSWORD': os.environ['RDS_PASSWORD'],
-        'HOST': os.environ['ALICANTE_HISTORY_HOST_DB'],
-        'PORT': os.environ['ALICANTE_HISTORY_PORT_DB'],
+        'NAME': os.environ.get('ALICANTE_HISTORY_NAME_DB'),
+        'USER': os.environ.get('ALICANTE_HISTORY_USER_DB'),
+        'PASSWORD': os.environ.get('ALICANTE_HISTORY_PASS_DB'),
+        'HOST': os.environ.get('ALICANTE_HISTORY_HOST_DB'),
+        'PORT': os.environ.get('ALICANTE_HISTORY_PORT_DB'),
     }
 }
 
-AWS_STORAGE_BUCKET_NAME = os.environ['AWS_STORAGE_BUCKET_NAME']
-AWS_S3_REGION_NAME = os.environ['AWS_S3_REGION_NAME']
-AWS_ACCESS_KEY_ID = os.environ['AWS_ACCESS_KEY_ID']
-AWS_SECRET_ACCESS_KEY = os.environ['AWS_SECRET_ACCESS_KEY']
+AWS_STORAGE_BUCKET_NAME = os.environ.get('AWS_STORAGE_BUCKET_NAME')
+AWS_S3_REGION_NAME = os.environ.get('AWS_S3_REGION_NAME')
+AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
+AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
 AWS_S3_CUSTOM_DOMAIN = '%s.s3.amazonaws.com' % AWS_STORAGE_BUCKET_NAME
 STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 STATICFILES_LOCATION = 'static'
