f994be7b983c15fdbab1e9f8428f38dd9f21d685
2paedev <dmartinezayuso@MacBook-Pro-de-Diego.local>
2019-12-05 19:47:10 +0100

    update pro settings

--- alicante_history/settings/pro.py
+++ alicante_history/settings/pro.py
@@ -2,9 +2,9 @@ import os
 
 from alicante_history.settings.base import *
 
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = ['*']
+ALLOWED_HOSTS = ['165.22.69.202']
 
 CORS_ORIGIN_ALLOW_ALL = True
 
@@ -18,14 +18,3 @@ DATABASES = {
         'PORT': os.environ.get('ALICANTE_HISTORY_PORT_DB'),
     }
 }
-
-AWS_STORAGE_BUCKET_NAME = os.environ.get('AWS_STORAGE_BUCKET_NAME')
-AWS_S3_REGION_NAME = os.environ.get('AWS_S3_REGION_NAME')
-AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
-AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
-AWS_S3_CUSTOM_DOMAIN = '%s.s3.amazonaws.com' % AWS_STORAGE_BUCKET_NAME
-STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
-STATICFILES_LOCATION = 'static'
-STATICFILES_STORAGE = 'custom_storages.StaticStorage'
-MEDIAFILES_LOCATION = 'media'
-DEFAULT_FILE_STORAGE = 'custom_storages.MediaStorage'
